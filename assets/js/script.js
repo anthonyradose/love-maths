ad56232cd7253c3f7544bcc8871ec0735c4b7c67
anthonyradose <asradose@gmail.com>
2023-10-30 18:15:21 +0000

    add code for subtract game

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,7 +27,10 @@ function runGame(gameType) {
 
     if (gameType === "add") {
         displayAdditionQuestion(num1, num2);
-    } else if (gameType === "multiply") {
+    } else if (gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
+    }
+    else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
     }
     else {
@@ -64,7 +67,9 @@ function calculateCorrectAnswer() {
     let operand2 = parseInt(document.getElementById('operand2').innerText);
     let operator = document.getElementById("operator").innerText;
     if (operator === "+") {
-        return [operand1 + operand2, "addition"];
+        return [operand1 + operand2, "add"];
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
     }
     else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
@@ -97,8 +102,10 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "+";
 }
 
-function displaySubtractQuestion() {
-
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand2 < operand1 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
