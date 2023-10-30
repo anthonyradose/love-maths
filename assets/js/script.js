2a3ff5711fd10b1e5ea0d0e1567232f1d77e15bb
anthonyradose <asradose@gmail.com>
2023-10-30 17:57:46 +0000

    Add multiplication code

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,7 +27,10 @@ function runGame(gameType) {
 
     if (gameType === "add") {
         displayAdditionQuestion(num1, num2);
-    } else {
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+    }
+    else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -62,13 +65,15 @@ function calculateCorrectAnswer() {
     let operator = document.getElementById("operator").innerText;
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
+    }
+    else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
 
 }
-
 
 /**
  * Gets the current score from the DOM and increments it by 1
@@ -96,6 +101,8 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
-
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 }
