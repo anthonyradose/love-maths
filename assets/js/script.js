638cbb890f0f264464298aff19e35aa32e2ffaa6
anthonyradose <asradose@gmail.com>
2023-10-31 12:45:30 +0000

    Add division code

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     }
-    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+    document.getElementById("answer-box").addEventListener("keydown", function (event) {
         if (event.key === "Enter") {
             checkAnswer();
         }
@@ -40,6 +40,9 @@ function runGame(gameType) {
     }
     else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
+    }
+    else if (gameType === "divide") {
+        displayDivisionQuestion(num1, num2);
     }
     else {
         alert(`Unknown game type: ${gameType}`);
@@ -81,6 +84,10 @@ function calculateCorrectAnswer() {
     }
     else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
+    }
+
+    else if (operator === "/") {
+        return [operand1 / operand2, "divide"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -120,4 +127,10 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 % operand2 === 0 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand2 % operand1 !== 0 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "/";
 }
